4f21aa7f60c440294e98e1a050492bd67fa6848d
Migle Kasetaite <kasetaite.m@gmail.com>
2022-08-26 13:26:57 +0200

    minor change in clustering.py file

--- IoTa/src/utilities/clustering.py
+++ IoTa/src/utilities/clustering.py
@@ -8,6 +8,7 @@ heuristics = ["h0", "h1", "h0 and h1", "h0 or h1"]
 
 
 def Clustering(file):
+    print('\n')
     print("CLUSTERING")
     for  i in heuristics:
         df = pd.read_csv(file)
@@ -19,6 +20,6 @@ def Clustering(file):
         cs_addr = sorted([g.vs.select(c)["name"] for c in cs], key=len)
         cs_sizes = [len(cs)]
 
-        print(i," - Number of Clusters:",cs_sizes)
+        print(i,"- Number of Clusters:",cs_sizes)
         print ("Addresses belonging to cluster:",cs_addr)
         print('\n')
