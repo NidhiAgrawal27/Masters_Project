a5281b3f82d47fe0235c566d85d4f17bb38c8327
Nidhi Agrawal <nidhiagrawal@vpn-10-21-52-055.uzh.ch>
2022-09-30 20:45:52 +0200

    added all datasets in run.sh

--- correspondence_network/src/run.sh
+++ correspondence_network/src/run.sh
@@ -1,24 +1,44 @@
-#!/bin/bash
 
-# for seed in {42..46}
-for seed in {42..42}
-    do
+# iota
+python -m mains.main_correspondence_ntwrk --seed 42 --currency iota --heuristic h0 --vis yes
+python -m mains.main_correspondence_ntwrk --seed 42 --currency iota --heuristic h0+h1 --vis yes
+python -m mains.main_network_analysis --seed 42 --currency iota --heuristic h0
+python -m mains.main_network_analysis --seed 42 --currency iota --heuristic h0+h1
 
-        # python -m mains.main_correspondence_ntwrk --seed $seed --currency iota --heuristic h0 --vis yes
-        # python -m mains.main_correspondence_ntwrk --seed $seed --currency iota --heuristic h0+h1 --vis yes
-        # python -m mains.main_network_analysis --seed $seed --currency iota --heuristic h0
-        # python -m mains.main_network_analysis --seed $seed --currency iota --heuristic h0+h1
+# # btc_sample
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency btc_sample --heuristic h0 --vis yes
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency btc_sample --heuristic h0+h1 --vis yes
+# python -m mains.main_network_analysis --seed 42 --currency btc_sample --heuristic h0
+# python -m mains.main_network_analysis --seed 42 --currency btc_sample --heuristic h0+h1
 
+# # btc_3GB_chunk
+# python -m mains.main_split_correspondence_ntwrk --seed 42 --currency btc_3GB_chunk --heuristic h0 --vis no --chunksize 1000000
+# python -m mains.main_split_correspondence_ntwrk --seed 42 --currency btc_3GB_chunk --heuristic h0+h1 --vis no --chunksize 1000000
+# python -m mains.main_network_analysis --seed 42 --currency btc_3GB_chunk --heuristic h0
+# python -m mains.main_network_analysis --seed 42 --currency btc_3GB_chunk --heuristic h0+h1
 
-        # python -m mains.main_correspondence_ntwrk --seed $seed --currency btc --heuristic h0 --vis yes
-        # python -m mains.main_correspondence_ntwrk --seed $seed --currency btc --heuristic h0+h1 --vis yes
-        # python -m mains.main_network_analysis --seed $seed --currency btc --heuristic h0
-        # python -m mains.main_network_analysis --seed $seed --currency btc --heuristic h0+h1
+# # btc
+# python -m mains.main_split_correspondence_ntwrk --seed 42 --currency btc --heuristic h0 --vis no --chunksize 1000000
+# python -m mains.main_split_correspondence_ntwrk --seed 42 --currency btc --heuristic h0+h1 --vis no --chunksize 1000000
+# python -m mains.main_network_analysis --seed 42 --currency btc --heuristic h0
+# python -m mains.main_network_analysis --seed 42 --currency btc --heuristic h0+h1
 
-        python -m mains.main_split_correspondence_ntwrk --seed $seed --currency iota --heuristic h0 --vis yes --chunksize 5000
-        python -m mains.main_split_correspondence_ntwrk --seed $seed --currency iota --heuristic h0+h1 --vis yes --chunksize 5000
-        python -m mains.main_network_analysis --seed $seed --currency iota --heuristic h0
-        python -m mains.main_network_analysis --seed $seed --currency iota --heuristic h0+h1
+# # cardano
+# python -m mains.main_split_correspondence_ntwrk --seed 42 --currency cardano --heuristic h0 --vis no  --chunksize 1000000
+# python -m mains.main_split_correspondence_ntwrk --seed 42 --currency cardano --heuristic h0+h1 --vis no  --chunksize 1000000
+# python -m mains.main_network_analysis --seed 42 --currency cardano --heuristic h0
+# python -m mains.main_network_analysis --seed 42 --currency cardano --heuristic h0+h1
 
-    done
+# # monacoin
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency monacoin --heuristic h0 --vis no
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency monacoin --heuristic h0+h1 --vis no
+# python -m mains.main_network_analysis --seed 42 --currency monacoin --heuristic h0
+# python -m mains.main_network_analysis --seed 42 --currency monacoin --heuristic h0+h1
+
+# # feathercoin
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency feathercoin --heuristic h0 --vis no
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency feathercoin --heuristic h0+h1 --vis no
+# python -m mains.main_network_analysis --seed 42 --currency feathercoin --heuristic h0
+# python -m mains.main_network_analysis --seed 42 --currency feathercoin --heuristic h0+h1
+
 
