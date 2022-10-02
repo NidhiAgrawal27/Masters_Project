2424824611c7c138b6c72f2972573ac267747cc8
Afshan Anam Saeed <afshananamsaeed@gmail.com>
2022-10-02 10:30:33 +0200

    Added main split for btc 2011s

--- correspondence_network/src/run.sh
+++ correspondence_network/src/run.sh
@@ -18,8 +18,8 @@
 # python -m mains.main_network_analysis --seed 42 --currency btc_3GB_chunk --heuristic h0+h1 --data_is_split yes
 
 # # btc_2011s
-# python -m mains.main_correspondence_ntwrk --seed 42 --currency btc_2011s --heuristic h0 --vis no
-# python -m mains.main_correspondence_ntwrk --seed 42 --currency btc_2011s --heuristic h0+h1 --vis no
+# python -m mains.main_split_correspondence_ntwrk --seed 42 --currency btc_2011s --heuristic h0 --vis no --chunksize 1000000
+# python -m mains.main_split_correspondence_ntwrk --seed 42 --currency btc_2011s --heuristic h0+h1 --vis no --chunksize 1000000
 # python -m mains.main_network_analysis --seed 42 --currency btc_2011s --heuristic h0 --data_is_split yes
 # python -m mains.main_network_analysis --seed 42 --currency btc_2011s --heuristic h0+h1 --data_is_split yes
 
