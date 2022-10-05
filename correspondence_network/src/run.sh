0e4e896f3230073a092ce8f40a53844199a863c6
Nidhi Agrawal <nidhiagrawal@vpn-10-21-49-073.uzh.ch>
2022-10-05 14:26:11 +0200

    new run.sh

--- correspondence_network/src/run.sh
+++ correspondence_network/src/run.sh
@@ -1,15 +1,15 @@
 
-# iota
-python -m mains.main_correspondence_ntwrk --seed 42 --currency iota --heuristic h0 --vis yes --data_is_split no
-python -m mains.main_correspondence_ntwrk --seed 42 --currency iota --heuristic h0+h1 --vis yes --data_is_split no
+# # iota
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency iota --heuristic h0 --vis yes --data_is_split no
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency iota --heuristic h0+h1 --vis yes --data_is_split no
 
-# iota_split
-python -m mains.main_correspondence_ntwrk --seed 42 --currency iota_split --heuristic h0 --vis yes --data_is_split no_modin_split --chunksize 1000
-python -m mains.main_correspondence_ntwrk --seed 42 --currency iota_split --heuristic h0+h1 --vis yes --data_is_split no_modin_split --chunksize 1000
+# # iota_split
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency iota_split --heuristic h0 --vis yes --data_is_split no_modin_split --chunksize 1000
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency iota_split --heuristic h0+h1 --vis yes --data_is_split no_modin_split --chunksize 1000
 
-# btc_sample
-python -m mains.main_correspondence_ntwrk --seed 42 --currency btc_sample --heuristic h0 --vis no --data_is_split no
-python -m mains.main_correspondence_ntwrk --seed 42 --currency btc_sample --heuristic h0+h1 --vis no --data_is_split no
+# # btc_sample
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency btc_sample --heuristic h0 --vis no --data_is_split no
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency btc_sample --heuristic h0+h1 --vis no --data_is_split no
 
 # # btc_2012
 # python -m mains.main_correspondence_ntwrk --seed 42 --currency btc_2012 --heuristic h0 --vis no --data_is_split no
