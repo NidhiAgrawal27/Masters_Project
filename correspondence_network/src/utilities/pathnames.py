d64226d156e464ec6a34f0e32382d93dc8a188a0
Nidhi Agrawal <nidhiagrawal@vpn-10-21-12-209.uzh.ch>
2022-11-07 22:05:52 +0100

    server path changed

--- correspondence_network/src/utilities/pathnames.py
+++ correspondence_network/src/utilities/pathnames.py
@@ -16,7 +16,7 @@ def set_seed(seed):
 def pathnames(cur, heuristic, weighted):
 
     local_dir = '../logs/' 
-    server_dir = '/local/scratch/correspondence_network/' 
+    server_dir = '/local/scratch/correspondence_network/part1_final_logs/'
 
     # below: change local_dir or server_dir in dir_name for accessing local or server dir
     if weighted == 'no': dir_name = local_dir + cur + '_logs/unweighted/' + heuristic + '/'
