c04d94a15cd8198c0fe6561bf9249c4b84747cf3
Nidhi Agrawal <nidhiagrawal@macbook-air.home>
2022-11-12 21:23:17 +0100

    font type

--- correspondence_network/src/utilities/visualization.py
+++ correspondence_network/src/utilities/visualization.py
@@ -1,6 +1,21 @@
 import matplotlib.pyplot as plt
 import powerlaw as pl
 import numpy as np
+import matplotlib as mpl
+
+uzh_color_map = ['#0028a5', '#dc6027', '#91c34a', '#fede00', '#a3adb7', '#0b82a0', '#2a7f62', # FULL
+                 '#667ec9', '#eaa07d', '#bfdf94', '#fcec7c', '#c8ced4', '#6bb7c7', '#80b6a4', # 60%
+                 '#3353b7', '#e38052', '#aad470', '#fbe651', '#b5bdc5', '#3c9fb6', '#569d85', # 80%
+                 '#99a9db', '#f1bfa9', '#d5e9b7', '#fdf3a8', '#dadee2', '#9ed0d9', '#abcec2', # 40%
+                 '#ccd4ed', '#f8dfd4', '#eaf4db', '#fef9d3', '#edeff1', '#cfe8ec', '#d5e7e1'] # 20%
+
+plt.rc('text', usetex=False)
+plt.rcParams['font.family'] = 'Microsoft Sans Serif'
+plt.rcParams['font.sans-serif'] = 'TheSans'
+plt.rcParams['axes.labelsize'] = 16
+plt.rcParams['font.size'] = 12
+mpl.rcParams['axes.linewidth'] = 1.2
+mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=uzh_color_map)
 
 
 def plot_density_graph(df, xlabel, fig_file_name, cur, heuristic):
@@ -9,9 +24,9 @@ def plot_density_graph(df, xlabel, fig_file_name, cur, heuristic):
     plt.hist(df, bins = bins_num, density = True, edgecolor='White')
     plt.xscale('log')
     plt.yscale('log')                            
-    plt.xlabel('Connected component size', fontsize=14)
-    plt.ylabel('Probability Density', fontsize=14)
-    plt.title(' '.join(cur.split('_')).capitalize() + ' ' + heuristic ,fontsize=15)
+    plt.xlabel('Connected component size')
+    plt.ylabel('Probability Density')
+    plt.title(' '.join(cur.split('_')).capitalize() + ' ' + heuristic)
     plt.savefig(fig_file_name, bbox_inches="tight")
     return
 
@@ -22,17 +37,15 @@ def plotPowerLaw(df, cur, heuristic, fig_file_name, xmin= None, xmax = None):
     alpha = fit.power_law.alpha
     xmin = fit.power_law.xmin
     if xmax is None: xmax = max(df)
-    # Plots the complementary cumulative distribution function (CDF) of the theoretical 
-    # distribution for the values given in data within xmin and xmax, if present
     fig = fit.plot_ccdf()
     fit.power_law.plot_ccdf( color= 'b',linestyle='--',label='fit ccdf',ax=fig)
     plt.xscale('log')
     plt.yscale('log')
-    plt.ylabel('Complementary cumulative distribution function 1 - P(c)', fontsize = 14)
-    plt.xlabel('\nConnected component size c', fontsize = 14)
+    plt.ylabel('Complementary cumulative distribution function 1 - P(c)')
+    plt.xlabel('\nConnected component size c')
     # plt.xlabel('\nNumber of addresses\nfit.distribution_compare(power_law, lognormal): '+ str(fit.distribution_compare('power_law', 'lognormal')), fontsize = 14)
     plt.title('PowerLaw Plot for ' + ' '.join(cur.split('_')).capitalize() + ' ' + 
-                    heuristic + '\n\u03B1 = %f in range [xmin, xmax] = [%.0f,%.0f]'%(alpha,xmin,xmax),fontsize=15)
+                    heuristic + '\n\u03B1 = %f in range [xmin, xmax] = [%.0f,%.0f]'%(alpha,xmin,xmax))
     plt.savefig(fig_file_name, bbox_inches="tight")
     return
 
@@ -42,9 +55,9 @@ def plot_modularity_graph(dataframe, community_property, title, fig_file_name):
     plt.scatter(x = dataframe["component_size"], y = dataframe[community_property])
     plt.xscale("log") 
     if not (community_property=="modularity"): plt.yscale("log") 
-    plt.xlabel('Connected component size', fontsize=14)
-    plt.ylabel(' '.join(community_property.split('_')).capitalize(), fontsize=14)
-    plt.title(title, fontsize=16)
+    plt.xlabel('Connected component size')
+    plt.ylabel(' '.join(community_property.split('_')).capitalize())
+    plt.title(title)
     plt.savefig(fig_file_name, bbox_inches="tight")
     return
 
