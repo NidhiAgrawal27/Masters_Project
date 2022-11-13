fce4eb6d84d20ee5a813ca7fe0aaa7f8bf7b6932
Nidhi Agrawal <nidhiagrawal@vpn-10-21-17-152.uzh.ch>
2022-11-13 17:14:10 +0100

    added cardano sample

--- correspondence_network/src/run.sh
+++ correspondence_network/src/run.sh
@@ -1,9 +1,9 @@
 
 # # iota_14days
-# python -m mains.main_correspondence_ntwrk --seed 42 --currency iota_14days --heuristic h0 --vis yes \
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency iota_14days --heuristic h0 --vis no \
 #                                             --data_is_split no --save_graph yes --load_graph no --weighted yes
                                             
-# python -m mains.main_correspondence_ntwrk --seed 42 --currency iota_14days --heuristic h0_h1 --vis yes \
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency iota_14days --heuristic h0_h1 --vis no \
 #                                             --data_is_split no --save_graph yes --load_graph no --weighted yes
 
 # # iota_split
@@ -37,6 +37,15 @@
 # python -m mains.main_correspondence_ntwrk --seed 42 --currency cardano --heuristic h0_h1 --vis no \
 #                                             --data_is_split yes --save_graph yes --load_graph no \
 #                                             --weighted no --chunksize 1000000
+
+# # cardano_sample
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency cardano_sample --heuristic h0 --vis no \
+#                                             --data_is_split no --save_graph yes --load_graph no \
+#                                             --weighted no
+
+# python -m mains.main_correspondence_ntwrk --seed 42 --currency cardano_sample --heuristic h0_h1 --vis no \
+#                                             --data_is_split no --save_graph yes --load_graph no \
+#                                             --weighted no
 
 # # monacoin
 # python -m mains.main_correspondence_ntwrk --seed 42 --currency monacoin --heuristic h0 --vis no \
