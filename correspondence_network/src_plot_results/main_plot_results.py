cefa5e8eb878935c3ac577b776b68a4f6d8254b8
Nidhi Agrawal <nidhiagrawal@macbook-air.home>
2022-11-24 23:25:39 +0100

    edited plots

--- correspondence_network/src_plot_results/main_plot_results.py
+++ correspondence_network/src_plot_results/main_plot_results.py
@@ -42,36 +42,26 @@ def main(load_dir, save_dir, currencies, weighted, modularity_file):
             plot_density_graph(df_components_h0['component_size'], 'Connected Component Size', save_fig_dir_h0 + 'density_plot.png', cur, 'h0')
             plot_density_graph(df_components_h0_h1['component_size'], 'Connected Component Size', save_fig_dir_h0_h1 + 'density_plot.png', cur, 'h0_h1')
 
-            plot_pdf(df_components_h0['component_size'], 'Connected Component Size', save_fig_dir_h0 + 'pdf_plot.png', cur, 'h0')
-            plot_pdf(df_components_h0_h1['component_size'], 'Connected Component Size', save_fig_dir_h0_h1 + 'pdf_plot.png', cur, 'h0_h1')
-
-            plot_probability(df_components_h0['component_size'], 'Connected Component Size', save_fig_dir_h0 + 'probability_plot.png', cur, 'h0')
-            plot_probability(df_components_h0_h1['component_size'], 'Connected Component Size', save_fig_dir_h0_h1 + 'probability_plot.png', cur, 'h0_h1')
-
             plot_modularity_graph(df_components_h0, "num_of_communities", title_h0, save_fig_dir_h0 + 'comp_size_communities.png')
             plot_modularity_graph(df_components_h0, "modularity", 'Modularity of ' + title_h0 + ' graph: ' + str(round(modularity, 4)), save_fig_dir_h0 + 'comp_size_modularity.png')
             
             plotPowerLaw_superimpose(df_components_h0['component_size'], df_components_h0_h1['component_size'], cur, 'h0', 'h0_h1', fig_path_h0 + cur + '_powerlaw_plot.png', xmin= None, xmax = None)
             
-            plot_gaussian(df_components_h0['component_size'], df_components_h0['num_of_edges'], cur, 'Connected Component Size', 'Number of Edges', save_fig_dir_h0 + 'edges.png')
-            plot_gaussian(df_components_h0_h1['component_size'], df_components_h0_h1['num_of_edges'], cur, 'Connected Component Size', 'Number of Edges', save_fig_dir_h0_h1 + 'edges.png')
+            plot_edges_gaussian(df_components_h0['component_size'], df_components_h0['num_of_edges'], cur, wt, 'Connected Component Size', 'Number of Edges', save_fig_dir_h0 + 'comp_size_edges.png')
+            plot_edges_gaussian(df_components_h0_h1['component_size'], df_components_h0_h1['num_of_edges'], cur, wt, 'Connected Component Size', 'Number of Edges', save_fig_dir_h0_h1 + 'comp_size_edges.png')
             
-
-            # We probably don't need these seperate functions
-            # plot_find_slope(df_components_h0, 'num_of_communities', title_h0, save_fig_dir_h0 + 'num_of_communities_plot.png')
-            # plot_densitygraph_vertical(df_components_h0, df_components_h0_h1, 'component_size', fig_path_h0 + cur + 'stacked_density_plot.png' , cur, 'h0', 'h0_h1')
 
 if __name__ == "__main__":
 
     # load_dir = '/local/scratch/correspondence_network/part1_final_logs/'
     # save_dir = '/local/scratch/correspondence_network/part1_plots/'
-    # modularity_file = '/local/scratch/correspondence_network/part1_plots/modularity_of_all_graphs.csv'
+    # modularity_file = '/local/scratch/correspondence_network/part1_final_logs/modularity_of_all_graphs.csv'
     
     load_dir = '/Users/nidhiagrawal/Desktop/Assignments/MastersProject/Github/FINAL/part1_final_logs/'
     save_dir = '../logs/plot_results/'
-    modularity_file = '../logs/modularity_of_all_graphs.csv'
+    modularity_file = load_dir + 'modularity_of_all_graphs.csv'
 
-    currencies = ['feathercoin', 'btc_sample']#, 'iota_14days', 'iota', 'monacoin']
+    currencies = ['feathercoin', 'btc_sample', 'iota_14days', 'iota', 'monacoin']
     weighted = ['weighted', 'unweighted']
     
     main(load_dir, save_dir, currencies, weighted, modularity_file)
