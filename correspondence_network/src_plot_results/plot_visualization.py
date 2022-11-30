7eb70cd05d69874947dc973c71f5e59960080de7
Nidhi Agrawal <nidhiagrawal@macbook-air.home>
2022-11-30 23:41:01 +0100

    included claudios changes..colours and power law changes

--- correspondence_network/src_plot_results/plot_visualization.py
+++ correspondence_network/src_plot_results/plot_visualization.py
@@ -14,6 +14,14 @@ uzh_color_map = ['#0028a5', '#dc6027', '#91c34a', '#fede00', '#a3adb7', '#0b82a0
                  '#99a9db', '#f1bfa9', '#d5e9b7', '#fdf3a8', '#dadee2', '#9ed0d9', '#abcec2', # 40%
                  '#ccd4ed', '#f8dfd4', '#eaf4db', '#fef9d3', '#edeff1', '#cfe8ec', '#d5e7e1'] # 20%
 
+uzh_colors = {'blue': '#0028a5', 'blue_80': '#3353b7', 'blue_60': '#667ec9', 'blue_40': '#99a9db', 'blue_20': '#ccd4ed',
+               'red': '#dc6027', 'red_80': '#e38052', 'red_60': '#eaa07d', 'red_40': '#f1bfa9', 'red_20': '#f8dfd4',
+               'green': '#91c34a', 'green_80': '#aad470', 'green_60': '#bfdf94', 'green_40': '#d5e9b7', 'green_20': '#eaf4db',
+               'yellow': '#fede00', 'yellow_80': '#fbe651', 'yellow_60': '#fcec7c', 'yellow_40': '#fdf3a8', 'yellow_20': '#fef9d3',
+               'grey': '#a3adb7', 'grey_80': '#b5bdc5', 'grey_60': '#c8ced4', 'grey_40': '#dadee2', 'grey_20': '#edeff1',
+               'turquoise': '#0b82a0', 'turquoise_80': '#3c9fb6', 'turquoise_60': '#6bb7c7', 'turquoise_40': '#9ed0d9', 'turquoise_20': '#cfe8ec',
+               'green2': '#2a7f62', 'green2_80': '#569d85', 'green2_60': '#80b6a4', 'green2_40': '#abcec2', 'green2_20': '#d5e7e1'}
+
 
 font_path = '../src/utilities/Fonts/TheSans Plain.otf'
 fm.fontManager.addfont(font_path)
@@ -34,31 +42,33 @@ mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=uzh_color_map)
 
 
 
-
 def plotPowerLaw_superimpose(df1, df2, cur, heuristic1, heuristic2, fig_file_name, xmin= None, xmax = None):
     plt.figure(figsize=(8,8))
     fit1 = pl.Fit(df1 , xmin, xmax = xmax)
     fit2 = pl.Fit(df2 , xmin, xmax = xmax)
     alpha1 = fit1.power_law.alpha
+    alpha1 = str(round(alpha1,2))
     xmin1 = fit1.power_law.xmin
     alpha2 = fit2.power_law.alpha
+    alpha2 = str(round(alpha2,2))
     xmin2 = fit2.power_law.xmin
     xmax1 = max(df1)
     xmax2 = max(df2)
-    fig1 = fit1.plot_ccdf()
-    fit1.power_law.plot_ccdf( color= 'r',linestyle='--',ax=fig1, label= heuristic1)
-    fig2 = fit2.plot_ccdf()
-    fit2.power_law.plot_ccdf( color= 'g',linestyle='-.',ax=fig2, label= heuristic2)
+    fig1 = fit1.plot_ccdf(color= uzh_colors["red"], label= heuristic1 + ', '+ r'$\alpha$ = ' + alpha1 + ' in range c = [%.0f, %.0f]'%(xmin1,xmax1))
+    # fit1.power_law.plot_ccdf( color= uzh_colors['red'],linestyle='--',ax=fig1, label= heuristic1)
+    fig2 = fit2.plot_ccdf(color= uzh_colors["green"], label= heuristic2 + ', '+ r'$\alpha$ = ' + alpha2 + ' in range c = [%.0f, %.0f]'%(xmin2,xmax2))
+    # fit2.power_law.plot_ccdf( color= uzh_colors["green"],linestyle='-.',ax=fig2, label= heuristic2)
     y_max_lim = max(fig1.properties()['ylim'][1], fig2.properties()['ylim'][1]) # get max value of y axis
     plt.ylim(0.0001, y_max_lim)
     plt.xscale('log')
     plt.yscale('log')
     plt.minorticks_off()
-    plt.ylabel('Complementary cumulative distribution function 1 - P(c)')
+    plt.ylabel('Complementary cumulative distribution function: 1 - P(c)')
     plt.xlabel('Connected component size c')
-    plt.title('PowerLaw Plot for: ' + ' '.join(cur.split('_')).capitalize() + ' ' + cur.capitalize() + 
-                '\n' + r'$\alpha$' + ' = %f for'%(alpha1)+ ' ' + heuristic1+' in range [xmin, xmax] = [%.0f, %.0f]'%(xmin1,xmax1)+ 
-                ' ' + 'and \n' + r'$\alpha$' + ' = %f for'%(alpha2)+ " " + heuristic2+ " " + 'in range [xmin, xmax] = [%.0f, %.0f]'%(xmin2,xmax2))
+    plt.title('PowerLaw Plot for: ' + ' '.join(cur.split('_')).capitalize())
+    # plt.title('PowerLaw Plot for: ' + ' '.join(cur.split('_')).capitalize() + ' ' + cur.capitalize() + 
+    #             '\n' + r'$\alpha$' + ' = ' + alpha1 + ' for ' + heuristic1+' in range of component size c = [%.0f, %.0f]'%(xmin1,xmax1)+ 
+    #             ' ' + 'and \n' + r'$\alpha$' + ' = ' + alpha2 + " for " + heuristic2+ " " + 'in range of component size c = [%.0f, %.0f]'%(xmin2,xmax2))
     plt.legend()
     plt.savefig(fig_file_name, bbox_inches="tight")
     plt.clf()
@@ -83,7 +93,7 @@ def plot_modularity_graph(dataframe, community_property, title, fig_file_name):
         Y = np.array(np.log(dataframe['num_of_communities']))
         slope, intercept  = np.polyfit(X, Y, 1)
         title = title + '\n' + ' Slope: ' + str(round(slope, 2)) + ' Intercept: ' + str(round(intercept, 2))
-        plt.plot(x, x/3, color='green', label = 'Theoretical max num of communities')
+        plt.plot(x, x/3, color=uzh_colors['green'], label = 'Theoretical max num of communities')
         plt.legend()
     if max(x) > 10 : plt.xscale("log")
     if max(y) > 10 :
@@ -94,6 +104,7 @@ def plot_modularity_graph(dataframe, community_property, title, fig_file_name):
     plt.savefig(fig_file_name, bbox_inches="tight")
     plt.clf()
     return
+
 
 
 def plot_edges_gaussian(x, y, cur, wt, xlabel, ylabel, fig_file_name):
@@ -124,6 +135,7 @@ def plot_edges_gaussian(x, y, cur, wt, xlabel, ylabel, fig_file_name):
     plt.savefig(fig_file_name, bbox_inches="tight")
     plt.clf()
     return
+
 
 
 
