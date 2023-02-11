dd0f7ef9a65e5116d41529841a17a64f3f4041a7
Nidhi Agrawal <nidhiagrawal@vpn-10-21-51-191.uzh.ch>
2023-02-11 01:14:09 +0100

    deleted logs

--- girvan_newmann_heuristic/src/run.sh
+++ girvan_newmann_heuristic/src/run.sh
@@ -1,11 +1,7 @@
+python -m mains.main_girvan_newmann --seed 42 --currency btc_sample --heuristic h0_h1 --weighted no
+python -m mains.main_girvan_newmann --seed 42 --currency btc_sample --heuristic h0_h1 --weighted yes
 
-# btc_sample unweighted
-python -m mains.main_girvan_newmann_heuristic --seed 42 --currency btc_sample --heuristic h0 --weighted no
-python -m mains.main_girvan_newmann_heuristic --seed 42 --currency btc_sample --heuristic h0_h1 --weighted no
-python -m mains.main_plot_metrics --seed 42 --currency btc_sample --weighted no
 
-# btc_sample weighted
-python -m mains.main_girvan_newmann_heuristic --seed 42 --currency btc_sample --heuristic h0 --weighted yes
-python -m mains.main_girvan_newmann_heuristic --seed 42 --currency btc_sample --heuristic h0_h1 --weighted yes
-python -m mains.main_plot_metrics --seed 42 --currency btc_sample --weighted yes
+# python -m mains.main_girvan_newmann --seed 42 --currency btc_2012_0 --heuristic h0_h1 --weighted no
+# python -m mains.main_girvan_newmann --seed 42 --currency btc_2012_0 --heuristic h0_h1 --weighted yes
 
